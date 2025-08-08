a8cfc434200ddc773e1cbe0f796ec0be06a4a78c
sagar7778 <koshtisagar7778@gmail.com>
2025-08-08 13:12:35 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,21 @@ export default async function handler(req: any, res: any) {
   if (!cachedServer) {
     const app = await NestFactory.create(AppModule);
 
-    const options = new DocumentBuilder()
+    const config = new DocumentBuilder()
       .setTitle('NestJS API')
       .setDescription('NestJS API description')
       .setVersion('1.0')
       .addTag('NestJS')
       .build();
 
-    const document = SwaggerModule.createDocument(app, options);
+    const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs', app, document);
 
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.use(cookieParser());
     app.enableCors({
       origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-      credentials: true,
+      credentials: true
     });
     app.setGlobalPrefix('api');
 
