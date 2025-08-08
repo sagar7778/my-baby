d638c9b85f491375a3eca55bb487f8ad5fc78dfa
sagar7778 <koshtisagar7778@gmail.com>
2025-08-08 12:16:45 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,35 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
-import * as cookieParser from 'cookie-parser'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const options = new DocumentBuilder()
-    .setTitle('NestJS API')
-    .setDescription('NestJS API description')
-    .setVersion('1.0')
-    .addTag('NestJS')
-    .build()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('docs', app, document)
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
-  app.use(cookieParser())
-  app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true
-  })
-  app.setGlobalPrefix('api')
-  await app.listen(process.env.PORT ?? 3000)
+let cachedServer: any;
+
+export default async function handler(req: any, res: any) {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+      .setTitle('NestJS API')
+      .setDescription('NestJS API description')
+      .setVersion('1.0')
+      .addTag('NestJS')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    app.use(cookieParser());
+    app.enableCors({
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+      credentials: true,
+    });
+    app.setGlobalPrefix('api');
+
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer(req, res);
 }
-bootstrap()
